aee03eb3abc49db72f8539cd7a9e604ac5d35d8e
Joohyun Kim <trust987@naver.com>
2023-11-26 17:56:21 +0900

    Feat: 카드 컴포넌트 경과 시간  캡션  기능 구현

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -6,6 +6,7 @@ import noImage from "../assets/no-img-logo.svg";
 const generateCreatedDate = (createAt) => {
   const createdDateAndTime = createAt.split("T");
   const date = createdDateAndTime[0].split("-");
+  const time = createdDateAndTime[1].split(":");
   const createdYear = date[0];
   const generateCreatedMonth = () => {
     if (date[1].indexOf("0") === -1) {
@@ -25,12 +26,77 @@ const generateCreatedDate = (createAt) => {
   };
   const createdMonth = generateCreatedMonth();
   const createdDay = generateCreatedDay();
-  return `${createdYear}. ${createdMonth}. ${createdDay}`;
+  const createdHours = time[0];
+  const createdMinutes = time[1];
+  return [createdYear, createdMonth, createdDay, createdHours, createdMinutes];
+};
+
+const calculateDateDiffWithCurrent = (date) => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const day = now.getDate();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  const targetDate = generateCreatedDate(date);
+  const targetYear = targetDate[0];
+  const targetMonth = targetDate[1];
+  const targetDay = targetDate[2];
+  const targetHours = targetDate[3];
+  const targetMinutes = targetDate[4];
+
+  const current = new Date(year, month, day, hours, minutes);
+  const target = new Date(targetYear, targetMonth, targetDay, targetHours, targetMinutes);
+  const timeDiffResult = current.getTime() - target.getTime();
+  return timeDiffResult;
+};
+
+const createTimePassedMessage = (date) => {
+  const timeDiff = calculateDateDiffWithCurrent(date);
+  const diffMinutes = Math.trunc(timeDiff / (1000 * 60));
+  const diffHours = Math.trunc(timeDiff / (1000 * 60 * 60));
+  const diffDays = Math.trunc(timeDiff / (1000 * 60 * 60 * 24));
+  const diffMonths = Math.trunc(timeDiff / (1000 * 60 * 60 * 24 * 30));
+  const diffYears = Math.trunc(timeDiff / (1000 * 60 * 60 * 24 * 30 * 12));
+
+  if (diffMinutes < 2) {
+    return "1 minute ago";
+  }
+  if (diffMinutes <= 59) {
+    return `${diffMinutes} minute ago`;
+  }
+  if (diffMinutes >= 60 && diffHours < 2) {
+    return "1 hour ago";
+  }
+  if (diffHours <= 23) {
+    return `${diffHours} hour ago`;
+  }
+  if (diffHours >= 24 && diffDays < 2) {
+    return "1 day ago";
+  }
+  if (diffDays <= 30) {
+    return `${diffDays} days ago`;
+  }
+  if (diffDays >= 31 && diffMonths < 2) {
+    return "1 month ago";
+  }
+  if (diffMonths <= 11) {
+    return `${diffMonths} months ago`;
+  }
+  if (diffMonths >= 12 && diffYears < 2) {
+    return "1 year ago";
+  }
+  if (diffYears >= 2) {
+    return `${diffYears} years ago`;
+  }
 };
 
 const Card = ({ item, isSelect, isStatic, onClick }) => {
   const { createdAt, description, imageSource, url } = item;
-  const createdDate = generateCreatedDate(createdAt);
+  const createdDate = generateCreatedDate(createdAt).slice(0, 3).join(".");
+  const timePassedMsg = createTimePassedMessage(createdAt);
   const handleCardClick = () => onClick(url);
   return (
     <div onClick={handleCardClick}>
@@ -42,7 +108,7 @@ const Card = ({ item, isSelect, isStatic, onClick }) => {
         )}
       </div>
       {isSelect ? <img src={selectedMark} alt="selected mark" /> : <img src={defaultMark} alt="unselected mark" />}
-      <p>{}</p>
+      <p>{timePassedMsg}</p>
       {!isStatic && <img src={kebab} alt="kebab" />}
       <p>{description}</p>
       <p>{createdDate}</p>
